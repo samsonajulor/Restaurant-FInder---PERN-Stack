8075d6816fe3da200743a3337085f29025e4d4a2
samsonajulor <samson.ajulor@decagon.dev>
2022-03-05 20:34:04 +0100

    created the update restaurant component

--- src/components/AddRestaurant.jsx
+++ src/components/AddRestaurant.jsx
@@ -16,7 +16,6 @@ const AddRestaurant = () => {
         location,
         price_range: priceRange,
       });
-      console.log(response.data.data);
       addRestaurants(response.data.data.restaurant);
     } catch (err) {
       console.log(err);
